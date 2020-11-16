22173eed67e3cb025d29896a6bffecae0e4ce104
AaronBackman <aaron.a.backman@gmail.com>
2020-11-16 18:48:34 +0200

    added gitignore to server folder

--- server/config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-// you need to add .env file for the environment variables
-
-exports.rootPassword = process.env.ROOT_PASSWORD || 'default';
-exports.username = process.env.DB_USERNAME || 'default';
-exports.userPassword = process.env.DB_PASSWORD || 'default';
-exports.databaseName = process.env.DATABASE_NAME || 'default';
